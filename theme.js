7d0d837fa4961231ff52e8bf49615b2cbae56f0b
trishnaravi <trishna.ravi91@gmail.com>
2023-01-17 16:56:17 -0500

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,3 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById("jenkins").innerHTML = "<div>HELLO</div>" + document.getElementById("jenkins").innerHTML;
+    document.getElementById("jenkins").innerHTML = "<div>One or more systems may be down. Click Here for more info <link></div>" + document.getElementById("jenkins").innerHTML;
 }, false);
